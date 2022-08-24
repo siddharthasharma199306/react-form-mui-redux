c191e2af181b1556a3b52eb1ed4ca2b96a7a4275
Siddhartha Sharma <siddhartha1993.06@gmail.com>
2022-08-24 22:05:29 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import userFormSchema from "./schemas/userFormSchema";
 
-function App() {
+export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+    reValidateMode: "onBlur",
+    resolver: yupResolver(userFormSchema),
+  });
+
+  console.count("app rerender");
+
+  const handleOnSubmit = (evt) => {
+    console.log(evt);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(handleOnSubmit)}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "100vh", minWidth: "100vw" }}
+      >
+        <Grid container>
+          <Grid item xs={12}>
+            <Controller
+              control={control}
+              name="username"
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  variant="filled"
+                  label="username"
+                  error={errors.username ? true : false}
+                  helperText={errors.username?.message}
+                  margin="normal"
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="age"
+              defaultValue=""
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label="Age With Validation"
+                  error={errors.age ? true : false}
+                  helperText={errors.age?.message}
+                  margin="normal"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="email"
+              defaultValue=""
+              render={({ field, fieldState: { error } }) => (
+                <TextField
+                  {...field}
+                  type="email"
+                  fullWidth
+                  label="Email With Validation"
+                  error={errors.email ? true : false}
+                  helperText={errors.email?.message}
+                  margin="normal"
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button type="submit">Submit</Button>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
-
-export default App;
---
+++ src/schemas/userFormSchema.js
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+const userFormSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  age: yup
+    .number()
+    .nullable()
+    .typeError("Age is required")
+    .required("Age is required")
+    .positive()
+    .integer()
+    .min(5, "Age should be greater than 5")
+    .max(21, "Age should under 22"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("Please enter a valid email"),
+});
+
+export default userFormSchema;
